7d4dbd2ccc3be81c6b118d436b88dcebb699c718
astragenius <darthmalak86@gmail.com>
2022-06-24 20:58:43 +0200

    add - add hero section img and text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './scss/index.scss'
 import logo from './assets/images/logo.png'
-
+import hero from './assets/images/hero-img.png'
 document.querySelector('.logo').src = logo
+document.querySelector('.hero-img').src = hero
