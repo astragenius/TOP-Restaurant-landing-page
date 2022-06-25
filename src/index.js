1208c24fe7104562f48d9a6eb3bf3d3ec80be194
astragenius <darthmalak86@gmail.com>
2022-06-25 14:32:18 +0200

    add - finished venue section styling startet testemonial styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import './scss/index.scss'
 import logo from './assets/images/logo.png'
 import hero from './assets/images/hero-img.png'
+import venue from './assets/images/venue-img.png'
+import prev1 from './assets/images/venue-preview-1.png'
+import prev2 from './assets/images/venue-preview-2.png'
+import prev3 from './assets/images/venue-preview-3.png'
+
 document.querySelector('.logo').src = logo
 document.querySelector('.hero-img').src = hero
+
+document.querySelector('.venue-img').src = venue
+document.querySelector('.preview-img1').src = prev1
+document.querySelector('.preview-img2').src = prev2
+document.querySelector('.preview-img3').src = prev3
