870de1d4a95f361b2a6414690a68ec8634a0872e
astragenius <darthmalak86@gmail.com>
2022-06-25 19:05:47 +0200

    style - finished layout and styling the menu page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import prev2 from './assets/images/venue-preview-2.png'
 import prev3 from './assets/images/venue-preview-3.png'
 import testemonialImg from './assets/images/testemonial-img.png'
 import testemonialImg2 from './assets/images/testemonial-img2.png'
+import menu1 from './assets/images/menu1.png'
+import menu2 from './assets/images/menu2.png'
+import menu3 from './assets/images/menu3.png'
+
+import menu4 from './assets/images/menu4.png'
+import menu5 from './assets/images/menu5.png'
+import menu6 from './assets/images/menu6.png'
+
+import menu7 from './assets/images/menu7.png'
+import menu8 from './assets/images/menu8.png'
+import menu9 from './assets/images/menu9.png'
 
 document.querySelector('.logo').src = logo
 document.querySelector('.hero-img').src = hero
@@ -17,3 +28,15 @@ document.querySelector('.preview-img2').src = prev2
 document.querySelector('.preview-img3').src = prev3
 document.getElementById('testemonial-img').src = testemonialImg
 document.querySelector('.testemonial-img').src = testemonialImg2
+
+document.querySelector('#menu1').src = menu1
+document.querySelector('#menu2').src = menu2
+document.querySelector('#menu3').src = menu3
+
+document.querySelector('#menu4').src = menu4
+document.querySelector('#menu5').src = menu5
+document.querySelector('#menu6').src = menu6
+
+document.querySelector('#menu7').src = menu7
+document.querySelector('#menu8').src = menu8
+document.querySelector('#menu9').src = menu9
