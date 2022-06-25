8570f17838bc627ce07f60ac50988a7dcbc546a8
astragenius <darthmalak86@gmail.com>
2022-06-25 15:17:22 +0200

    style - finished testemonial section

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import venue from './assets/images/venue-img.png'
 import prev1 from './assets/images/venue-preview-1.png'
 import prev2 from './assets/images/venue-preview-2.png'
 import prev3 from './assets/images/venue-preview-3.png'
+import testemonialImg from './assets/images/testemonial-img.png'
+import testemonialImg2 from './assets/images/testemonial-img2.png'
 
 document.querySelector('.logo').src = logo
 document.querySelector('.hero-img').src = hero
@@ -13,3 +15,5 @@ document.querySelector('.venue-img').src = venue
 document.querySelector('.preview-img1').src = prev1
 document.querySelector('.preview-img2').src = prev2
 document.querySelector('.preview-img3').src = prev3
+document.getElementById('testemonial-img').src = testemonialImg
+document.querySelector('.testemonial-img').src = testemonialImg2
