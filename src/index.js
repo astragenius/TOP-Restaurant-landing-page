70fa2619dee82469bb75617512c3a85fc4d57018
astragenius <darthmalak86@gmail.com>
2022-06-26 14:36:29 +0200

    add - finished Menu DomRender module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import './scss/index.scss'
 import { Home } from './js/Home'
+import { Menu } from './js/Menu'
 
 /* IMAGE IMPORTS */
+
 import logo from './assets/images/logo.png'
-//import hero from './assets/images/hero-img.png'
-import venue from './assets/images/venue-img.png'
-import prev1 from './assets/images/venue-preview-1.png'
-import prev2 from './assets/images/venue-preview-2.png'
-import prev3 from './assets/images/venue-preview-3.png'
-import testemonialImg from './assets/images/testemonial-img.png'
-import testemonialImg2 from './assets/images/testemonial-img2.png'
-import menu1 from './assets/images/menu1.png'
-import menu2 from './assets/images/menu2.png'
-import menu3 from './assets/images/menu3.png'
-
-import menu4 from './assets/images/menu4.png'
-import menu5 from './assets/images/menu5.png'
-import menu6 from './assets/images/menu6.png'
-
-import menu7 from './assets/images/menu7.png'
-import menu8 from './assets/images/menu8.png'
-import menu9 from './assets/images/menu9.png'
 
 import story1 from './assets/images/storyIMG1.png'
 import story2 from './assets/images/storyIMG2.png'
@@ -31,25 +15,6 @@ import map from './assets/images/map-img.png'
 
 document.querySelector('.logo').src = logo
 
-/* document.querySelector('.venue-img').src = venue
-document.querySelector('.preview-img1').src = prev1
-document.querySelector('.preview-img2').src = prev2
-document.querySelector('.preview-img3').src = prev3
-document.getElementById('testemonial-img').src = testemonialImg
-document.querySelector('.testemonial-img').src = testemonialImg2 */
-
-document.querySelector('#menu1').src = menu1
-document.querySelector('#menu2').src = menu2
-document.querySelector('#menu3').src = menu3
-
-document.querySelector('#menu4').src = menu4
-document.querySelector('#menu5').src = menu5
-document.querySelector('#menu6').src = menu6
-
-document.querySelector('#menu7').src = menu7
-document.querySelector('#menu8').src = menu8
-document.querySelector('#menu9').src = menu9
-
 document.querySelector('#storyImg1').src = story1
 document.querySelector('#storyImg2').src = story2
 
@@ -59,5 +24,4 @@ const homeBtn = document.getElementById('home-link')
 const menuBtn = document.getElementById('menu-link')
 const aboutUsBtn = document.getElementById('about-link')
 const content = document.getElementById('content')
-
-content.appendChild()
+content.appendChild(Menu.renderDOM())
---
+++ src/js/Menu.js
@@ -0,0 +1,76 @@
+import menu1 from '../assets/images/menu1.png'
+import menu2 from '../assets/images/menu2.png'
+import menu3 from '../assets/images/menu3.png'
+
+import menu4 from '../assets/images/menu4.png'
+import menu5 from '../assets/images/menu5.png'
+import menu6 from '../assets/images/menu6.png'
+
+import menu7 from '../assets/images/menu7.png'
+import menu8 from '../assets/images/menu8.png'
+import menu9 from '../assets/images/menu9.png'
+
+const Menu = (function () {
+    const menuImgs = [
+        menu1,
+        menu2,
+        menu3,
+        menu4,
+        menu5,
+        menu6,
+        menu7,
+        menu8,
+        menu9,
+    ]
+    const menuNames = [
+        'Bukhari',
+        'Saudi Makboos',
+        'Zurbiam',
+        'Kabsa',
+        'Madfoon',
+        'Math Lootha',
+        'Margoug',
+        'Sayadaya',
+        'Saleeg',
+    ]
+    const main = document.createElement('main')
+    main.classList.add('main-menu-container', 'wrapper')
+
+    const titelSection = document.createElement('section')
+    titelSection.classList.add('titel', 'section--spacing')
+    titelSection.innerHTML = `
+    <h2>Discover Our menu</h2>
+    <p>There is a wide selection of choices served by top Saudi chefs, prepared using best quality local ingredients.</p>`
+
+    const menuSection = document.createElement('section')
+    menuSection.classList.add('menu', 'section--spacing')
+
+    let menuGrid = document.createElement('div')
+    menuGrid.classList.add('menu-grid')
+
+    for (let i = 0; i < 9; i++) {
+        menuGrid.innerHTML += `<figure class="menu-box">
+                            <img id="menu1" src="${menuImgs[i]}" alt="Image of ${menuNames[i]}">
+                            <figcaption>
+                                ${menuNames[i]}
+                            </figcaption>
+                        </figure>`
+    }
+    menuSection.appendChild(menuGrid)
+
+    const btnContainer = document.createElement('div')
+    btnContainer.classList.add('menu-btn-container')
+    btnContainer.innerHTML = `<button class="btn btn-menu">Show more</button>`
+
+    function renderDOM() {
+        main.appendChild(titelSection)
+        main.appendChild(menuSection)
+        menuSection.appendChild(btnContainer)
+
+        return main
+    }
+
+    return { renderDOM }
+})()
+
+export { Menu }
